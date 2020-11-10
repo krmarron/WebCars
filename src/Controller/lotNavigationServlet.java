a023040982bd459161672072de4cce6aa5fc8420
krmarron <krmarron@dmacc.edu>
2020-11-10 10:47:51 -0600

    Added form validation to index.html for addCarServlet

--- src/Controller/lotNavigationServlet.java
+++ src/Controller/lotNavigationServlet.java
@@ -12,7 +12,7 @@ import Model.LotDetails;
 /**
  * Servlet implementation class lotNavigationServlet
  */
-@WebServlet("/lotNavigationServlet")
+@WebServlet("/lotnavigationServlet")
 public class lotNavigationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
